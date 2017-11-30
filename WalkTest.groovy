5108abee3ebaa5438b587ebe797db4576e520a66
Kevin Harrington <mad.hephaestus@gmail.com>
2017-11-30 11:34:09 -0500

    rotating walking

--- WalkTest.groovy
+++ WalkTest.groovy
@@ -3,7 +3,7 @@ import  com.neuronrobotics.bowlerstudio.physics.*;
 import com.neuronrobotics.bowlerstudio.threed.*;
 
 MobileBase base;
-Object dev = DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot");
+Object dev = DeviceManager.getSpecificDevice(MobileBase.class, "jaguar");
 println "found: "+dev
 //Check if the device already exists in the device Manager
 if(args==null){
@@ -31,20 +31,26 @@ if(args==null){
 	base=(MobileBase)args.get(0)
 
 println "walk forward 10 increments of 10 mm totalling 100 mm translation"
-TransformNR move = new TransformNR(10,0,0,new RotationNR())
-double toSeconds=0.1//100 ms for each increment
+TransformNR move = new TransformNR(5,0,0,new RotationNR())
+double toSeconds=0.03//100 ms for each increment
 for(int i=0;i<10;i++){
 	base.DriveArc(move, toSeconds);
 	ThreadUtil.wait((int)toSeconds*1000)
 }
+println "Waiting for legs to reset"
+ThreadUtil.wait(6000)// wait for the legs to fully reset themselves.
+
 println "turn 20 increments of 2 degrees totalling 40 degrees turn"
-move = new TransformNR(0,0,0,new RotationNR( 0,0, 2))
+move = new TransformNR(0,0,0,new RotationNR( 0,5,0))
 for(int i=0;i<20;i++){
 	base.DriveArc(move, toSeconds);
 	ThreadUtil.wait((int)toSeconds*1000)
 }
+println "Waiting for legs to reset"
+ThreadUtil.wait(6000)// wait for the legs to fully reset themselves.
+
 println "walk sideways 10 increments of 10 mm totalling 100 mm translation"
-move = new TransformNR(0,10,0,new RotationNR())
+move = new TransformNR(0,5,0,new RotationNR())
 for(int i=0;i<10;i++){
 	base.DriveArc(move, toSeconds);
 	ThreadUtil.wait((int)toSeconds*1000)
