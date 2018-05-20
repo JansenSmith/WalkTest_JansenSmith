837140f6656191bfa1533e5406fd1e69074e2abf
Kevin Harrington <mad.hephaestus@gmail.com>
2018-05-20 13:37:58 -0400

    modern base loading

--- WalkTest.groovy
+++ WalkTest.groovy
@@ -2,33 +2,24 @@ import org.apache.commons.io.IOUtils;
 import  com.neuronrobotics.bowlerstudio.physics.*;
 import com.neuronrobotics.bowlerstudio.threed.*;
 
-MobileBase base;
-Object dev = DeviceManager.listConnectedDevice(MobileBase.class);
-println "found: "+dev
+def base;
 //Check if the device already exists in the device Manager
 if(args==null){
-	if(dev.size()==0){
-		//Create the kinematics model from the xml file describing the D-H compliant parameters. 
-		def file=["https://github.com/madhephaestus/carl-the-hexapod.git","CarlTheRobot.xml"]as String[]
-		String xmlContent = ScriptingEngine.codeFromGit(file[0],file[1])[0]
-		MobileBase mb =new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"))
-		mb.setGitSelfSource(file)
-		mb.connect()
-		DeviceManager.addConnection(mb,mb.getScriptingName())
-		base=mb
-		println "Waiting for cad to generate"
-		ThreadUtil.wait(1000)
-		while(MobileBaseCadManager.get( base).getProcesIndictor().getProgress()<1){
-			println "Waiting for cad to get to 1:"+MobileBaseCadManager.get( base).getProcesIndictor().getProgress()
-			ThreadUtil.wait(1000)
-		}
-	}else{
-		println "Robot found, runing code"
-		//the device is already present on the system, load the one that exists.
-	  	base=DeviceManager.getSpecificDevice(dev.get(0))
-	}
+	base=DeviceManager.getSpecificDevice( "CarlTheWalkingRobot",{
+			//If the device does not exist, prompt for the connection
+			
+			MobileBase m = MobileBaseLoader.fromGit(
+				"https://github.com/madhephaestus/carl-the-hexapod.git",
+				"CarlTheRobot.xml"
+				)
+			if(m==null)
+				throw new RuntimeException("Arm failed to assemble itself")
+			println "Connecting new device robot arm "+m
+			return m
+		})
 }else
-	base=(MobileBase)args.get(0)
+	base=args.get(0)
+
 
 println "walk forward 10 increments of 10 mm totalling 100 mm translation"
 TransformNR move = new TransformNR(5,0,0,new RotationNR())
